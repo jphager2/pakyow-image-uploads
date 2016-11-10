142b5e38fccd0f3ccd55f1516aa0b094bfbb4943
jphager2 <jphager2@gmail.com>
2016-11-10 20:03:44 +0100

    add binding for uploaded images

--- app/lib/bindings.rb
+++ app/lib/bindings.rb
@@ -1,3 +1,10 @@
 Pakyow::App.bindings do
   # define bindings here
+  scope :'uploaded-image' do
+    binding :file do
+      part :src do
+        bindable.file.thumb.url if bindable.file.present?
+      end
+    end
+  end
 end
