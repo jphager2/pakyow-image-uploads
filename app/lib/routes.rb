97e3323fcd5ad72a6ce9aeca488c1021c0ac8e7f
jphager2 <jphager2@gmail.com>
2016-11-10 20:04:28 +0100

    add routes

--- app/lib/routes.rb
+++ app/lib/routes.rb
@@ -1,6 +1,18 @@
 Pakyow::App.routes do
   default do
-    logger.info 'hello'
+    view.scope(:image).bind({}) do |view, data|
+      view.attrs.action = router.group(:images).path(:create)
+      view.attrs.method = 'POST'
+    end
+    view.scope(:'uploaded-image').apply(Image.all)
+  end
+
+  namespace :images, 'images' do
+    post :create, '/' do
+      Image.create(params[:image])
+
+      redirect('/')
+    end
   end
 
   # define application routes here
